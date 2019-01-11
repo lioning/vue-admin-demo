8dc1addca5ef351db16b38e0f8b6e5015ea9b034
lioning <liang0356@sina.com>
2019-01-11 16:11:16 +0800

    注释掉 *.do 的 servlet 注册，否则匹配上 do_login.do 导致无法登录

--- StoreSystem/src/com/in28minutes/filter/LoginRequiredFilter.java
+++ StoreSystem/src/com/in28minutes/filter/LoginRequiredFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
-@WebFilter(urlPatterns = "*.do")
+//@WebFilter(urlPatterns = "*.do")
 public class LoginRequiredFilter implements Filter {
 
 	@Override
